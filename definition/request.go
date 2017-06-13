27e223525f1d3ad2ebfc724f663985a27b716389
Ricardo Lopes <ricardo.lopes@rocket-internet.de>
2017-06-13 18:29:51 +0200

    fixes some issues in apib template default, as in showing empty requests or not displaying body properly from examples

--- definition/request.go
+++ definition/request.go
@@ -7,3 +7,16 @@ type Request struct {
 	Body        []Body
 	Headers     []Header
 }
+
+// IsEmpty verifies is the request is empty
+func (r Request) IsEmpty() bool {
+	if r.Title != "" || r.Description != "" {
+		return false
+	}
+
+	if len(r.Body) > 0 || len(r.Headers) > 0 {
+		return false
+	}
+
+	return true
+}
